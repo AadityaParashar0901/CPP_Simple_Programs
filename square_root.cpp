56b242615113ba775eedd3811cc3257c1c6d4e8a
Aaditya Parashar <aaditya.p0901@gmail.com>
2025-04-28 00:37:03 +0530

    Update

--- square_root.cpp
+++ square_root.cpp
@@ -1,30 +1,17 @@
 #include <iostream>
-
 using namespace std;
-
 class NegativeNumber {};
-
-float approxSquareRoot (float t) {
+double approxSquareRoot (double t) {
 	if (t < 0) throw (NegativeNumber());
-	float number = 0, d = 1;
-	float product = 0;
-	while (t - product > 0.001) {
-		product = (number + d) * (number + d);
-		if (product > t) d /= 10;
+	double number = 0, d = 1;
+	double product = 0;
+	while (t - product > 0.000001) {
+		while ((number + d) * (number + d) > t) d /= 10;
 		number += d;
 		product = number * number;
 	}
-	return number;
-}
-
-int main() {
-	float n;
-	cout << "Enter a Number: ";
-	cin >> n;
-	try {
-		cout << approxSquareRoot(n) << endl;
-	} catch (NegativeNumber) {
-		cout << "Number cannot be negative\n";
-	}
-	return 0;
-}
+	return number; }
+int main() { double n;
+	cout << "Enter a Number: "; cin >> n;
+	try { cout << approxSquareRoot(n) << endl; } catch (NegativeNumber) { cout << "Number cannot be negative\n"; }
+	return 0; }
